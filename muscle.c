ba55cb2b7d45cee83dcbce2418d1f84041feba4c
Hemapriyasankar76 <164179514+Hemapriyasankar76@users.noreply.github.com>
2024-09-30 22:25:36 +0530

    Create muscle.c

---
+++ muscle.c
@@ -0,0 +1,14 @@
+
+const int muscleSensorPin = A0; // Analog pin where the muscle sensor is connected
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communication for debugging
+  while(!Serial){}
+}
+
+void loop() {
+  // Read analog value from muscle sensor
+  int muscleSensorValue = analogRead(muscleSensorPin);
+  
+  delay(1000); // Wait for a short duration before the next reading
+}
